d87749e8c05d6793f9a6677777d0f0f42c356c7b
devinair14 <44498633+devinair14@users.noreply.github.com>
2019-02-27 12:18:33 +0530

    Add files via upload

---
+++ time.c
@@ -0,0 +1,16 @@
+/*program to print 24 hours of a day
+author:devi nair
+date:20/02/2019*/
+#include<stdio.h>
+void main()
+{
+	int i;
+	printf("12 Midnight\n");
+	for(i=1;i<12;i++)
+		printf("%d A.M\n",i);
+	printf("12 Noon\n");
+	for(i=1;i<12;i++)
+		printf("%d P.M\n",i);
+}
+	
+	
